556bcf58f7797e5710e4bc9a2f8e60cb37af1e4a
Gabriel Lara Baptista <gabriel.l.baptista@gmail.com>
2023-03-19 20:27:07 -0300

    checking-platform sample

---
+++ ch02/checking-platform/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    Console.WriteLine("Here you have Windows World!");
+else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    Console.WriteLine("Here you have Linux World!");
+else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+    Console.WriteLine("Here you have macOS World!");
