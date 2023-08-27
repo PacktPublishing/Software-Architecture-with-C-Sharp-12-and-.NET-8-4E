70a19817e3122ec659abd29b59f1b553805f60af
Gabriel Lara Baptista <gabriel.l.baptista@gmail.com>
2023-08-27 19:24:50 -0300

    Appendix code

---
+++ appendix/Recommendation/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recommendation
+{
+    public class Rating
+    {
+        public int RateId { get; set; }
+        public int UserId { get; set; }
+        public int PlaceIdOrigin { get; set; }
+        public int PlaceIdDestiny { get; set; }
+        public float RatingValue {  get; set; }
+    }
+}
